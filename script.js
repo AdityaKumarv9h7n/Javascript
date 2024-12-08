deeea26106928e131ad66d6d4f172e137ffa0230
Astray Youth <122166694+AstrayYouth@users.noreply.github.com>
2024-12-08 19:42:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -772,3 +772,22 @@ console.log(`${ageMine >= 18 ? "You can drink anything" : "You can only drink wa
 // Inside of a tempelate literal you can use the conditional becase the ternary operator has a value as we used in the above code  
 
 // in the above example in the placeholder we used the expression ageMine >= 18 ? "You can drink anything" : "You can only drink water" because the expression has a value which can be  "You can drink anything" or "You can only drink water"  for different condition
+
+// Coading Challenge #4
+/* Steve wants to build a very simple tip calculator for whenever he goes eating in a restutant. In his country, it's usual to tip 15% if the bill value is between 50 and 300 If the value is different, the tip is 20%.
+
+1) Your task is to calculatae the tip depeding on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator! ) 
+
+2) Print a string to the cosole containing the bill value, the tip, and the final value (bill + tip). 
+Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
+
+TEST DATA: Test for bill values 275, 40 and 430.
+
+*/ 
+let billValue = 430;
+let tipValue = (billValue >= 50 && billValue <= 300) ? `${billValue*0.15}`: `${billValue*0.2}`;
+
+const total = (Number(tipValue) + Number(billValue));
+
+console.log(`The bill was ${billValue} ,the tip was ${tipValue} , and the total value is ${total}`);
+
