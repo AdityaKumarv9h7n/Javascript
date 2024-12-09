ed6d892d90e4d9cfe52b3e744928bb5748038100
Astray Youth <122166694+AstrayYouth@users.noreply.github.com>
2024-12-09 22:34:46 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,4 +1,34 @@
 // Javascript Fundamental Part 2
-
+'use strict'
 
 //Activating Strict Mode
+// in the earlier version of the javascript if you want to get know about any error like in which line of the code you did something wrong then you had to write the code in the strict mode which can be turn on just by writing 'use strict' in the first line of the begining of you js code 
+
+
+let nameOf = "Name";
+let lastName = "Kumar";
+console.log(nameOf + lastName);
+
+// but now after using the 'use strict' which is the strict mode you can now get a message into the console about in which line of the code you made a mistake 
+
+
+nameOf = "Name";
+lastName = "Kumar";
+console.log(nameOf + lastName);
+
+
+
+
+
+// strict mode actually helps you not to assign the variable which will be a feature in the upcoming versions of the JS like the variable interface
+
+
+//let interface = "name";
+//console.log(interface);
+
+// after adding the interface as a variable and using the strict mode which is on the top of all these codes actually help us not to assign some variable name which may be will be a feature in the upcoming js update those are called the reserved keywords
+
+// we have to move forward so i am going to comment out the above code of the let interface variable 
+
+// similarly we can not assign the variable name as private
+
